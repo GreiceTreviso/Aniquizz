908292dd86e5038930977c5354f9916624ebaa8a
Isabelly Carvalho Piccinato <isabellpiccinato@gmail.com>
2026-09-09 16:03:30 -0300

    commit 1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const numeroAleatorio = Math.random()
+
+const n1 = math.floor(2.9)
+
+const n2 = math.ceil(2.9)
+
+const n2 = math.round(2.5)
+
+console.log(n1) 
+console.log(n1) 
+console.log(n1) 
+
+alert("NUmero sorteado: " + numeroAleatorio)
