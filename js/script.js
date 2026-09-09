46ce7ca98f019bf1f078edbdf89d936d9faa445a
Isabelly Carvalho Piccinato <isabellpiccinato@gmail.com>
2026-09-09 16:10:41 -0300

    commit 2
    adicionado o conteudo aprendido em sala

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
-const numeroAleatorio = Math.random()
+//const numeroAleatorio = Math.random()
 
-const n1 = math.floor(2.9)
+const numeroAleatorio = Math.floor(Math.random()*6)+1
+console.log("Número sorteado: "+ numeroAleatorio)
 
-const n2 = math.ceil(2.9)
+const n1 = Math.floor(2.9)
+const n2 = Math.ceil(2.9)
+const n3 = Math.round(2.4) // .5 
 
-const n2 = math.round(2.5)
+console.log(n1) //2
+console.log(n2) //3
+console.log(n3) //
 
-console.log(n1) 
-console.log(n1) 
-console.log(n1) 
+for(let i=0;i<=5;i++){
+    const carta = document.createElement("div")
+    const texto = document.createElement("p")
 
-alert("NUmero sorteado: " + numeroAleatorio)
+    texto.textContent = `${numeroAleatorio}`
+    carta.appendChild(texto)
+    carta.classList.add('cartaFormatada')
+
+    document.querySelector("#tudo").appendChild(carta)
+    //alert("Número sorteado: "+ numeroAleatorio)
+}
