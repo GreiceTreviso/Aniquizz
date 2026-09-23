b27257f5136680731b56d25dd2f948eb52657248
GreiceTreviso <medicoerro@gmail.com>
2026-09-23 16:24:25 -0300

    commit

--- js/json.js
+++ js/json.js
@@ -80,6 +80,87 @@ const cartas = [
           "id": 16,
           "nome": "Yuta",
           "image": "assets/img/imgAnime/yuta.jpeg"
-        }
+        },
+        
+        {
+            "id": 17,
+            "nome": "Choso",
+            "image": "assets/img/imgAnime/choso.jpeg"
+          },
+          {
+            "id": 18,
+            "nome": "Deku",
+            "image": "assets/img/imgAnime/deku.jpeg"
+          },
+          {
+            "id": 19,
+            "nome": "Elric",
+            "image": "assets/img/imgAnime/elric.jpeg"
+          },
+          {
+            "id": 20,
+            "nome": "Frieren",
+            "image": "assets/img/imgAnime/frieren.jpeg"
+          },
+          {
+            "id": 21,
+            "nome": "Itachi",
+            "image": "assets/img/imgAnime/itachi.jpeg"
+          },
+          {
+            "id": 22,
+            "nome": "Kaguya",
+            "image": "assets/img/imgAnime/kaguya.jpeg"
+          },
+          {
+            "id": 23,
+            "nome": "Mahito",
+            "image": "assets/img/imgAnime/mahito.jpeg"
+          },
+          {
+            "id": 24,
+            "nome": "Marin",
+            "image": "assets/img/imgAnime/marin.jpeg"
+          },
+          {
+            "id": 25,
+            "nome": "Milk",
+            "image": "assets/img/imgAnime/milk.jpeg"
+          },
+          {
+            "id": 26,
+            "nome": "Nami",
+            "image": "assets/img/imgAnime/nami.jpeg"
+          },
+          {
+            "id": 27,
+            "nome": "Nezuko",
+            "image": "assets/img/imgAnime/nezuko.jpeg"
+          },
+          {
+            "id": 28,
+            "nome": "Saiki",
+            "image": "assets/img/imgAnime/saiki.jpeg"
+          },
+          {
+            "id": 29,
+            "nome": "Saitama",
+            "image": "assets/img/imgAnime/saitama.jpeg"
+          },
+          {
+            "id": 30,
+            "nome": "Todo",
+            "image": "assets/img/imgAnime/todo.jpeg"
+          },
+          {
+            "id": 31,
+            "nome": "Toga",
+            "image": "assets/img/imgAnime/toga.jpeg"
+          },
+          {
+            "id": 32,
+            "nome": "Yuji",
+            "image": "assets/img/imgAnime/yuji.jpeg"
+          }
       ]
 ]
