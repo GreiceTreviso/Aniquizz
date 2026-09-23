64d427f6a5360f33e0e92a92dd1ad00c71034861
GreiceTreviso <medicoerro@gmail.com>
2026-09-23 16:14:24 -0300

    commit

---
+++ js/json.js
@@ -0,0 +1,85 @@
+const cartas = [
+
+    [
+        {
+          "id": 1,
+          "nome": "David",
+          "image": "assets/img/imgAnime/david.jpeg"
+        },
+        {
+          "id": 2,
+          "nome": "Freez",
+          "image": "assets/img/imgAnime/freez.jpeg"
+        },
+        {
+          "id": 3,
+          "nome": "Goku",
+          "image": "assets/img/imgAnime/goku.jpeg"
+        },
+        {
+          "id": 4,
+          "nome": "Kira",
+          "image": "assets/img/imgAnime/kira.jpeg"
+        },
+        {
+          "id": 5,
+          "nome": "L",
+          "image": "assets/img/imgAnime/L.jpeg"
+        },
+        {
+          "id": 6,
+          "nome": "Levi",
+          "image": "assets/img/imgAnime/levi.jpeg"
+        },
+        {
+          "id": 7,
+          "nome": "Mark",
+          "image": "assets/img/imgAnime/mark.jpeg"
+        },
+        {
+          "id": 8,
+          "nome": "Mikasa",
+          "image": "assets/img/imgAnime/mikasa.jpeg"
+        },
+        {
+          "id": 9,
+          "nome": "Miku",
+          "image": "assets/img/imgAnime/miku.jpeg"
+        },
+        {
+          "id": 10,
+          "nome": "Obito",
+          "image": "assets/img/imgAnime/obito.jpeg"
+        },
+        {
+          "id": 11,
+          "nome": "Rebecca",
+          "image": "assets/img/imgAnime/rebecca.jpeg"
+        },
+        {
+          "id": 12,
+          "nome": "Ryu",
+          "image": "assets/img/imgAnime/ryu.jpeg"
+        },
+        {
+          "id": 13,
+          "nome": "Sarakura",
+          "image": "assets/img/imgAnime/sarakura.jpeg"
+        },
+        {
+          "id": 14,
+          "nome": "Shadow",
+          "image": "assets/img/imgAnime/shadow.jpeg"
+        },
+        {
+          "id": 15,
+          "nome": "Sonic",
+          "image": "assets/img/imgAnime/sonic.jpeg"
+        },
+        {
+          "id": 16,
+          "nome": "Yuta",
+          "image": "assets/img/imgAnime/yuta.jpeg"
+        }
+      ]
+]
